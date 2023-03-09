347e38ac1bb008778348f3e3b81c376afe68e33c
Ilkin Ocaqverdiyev <ocaqverdiyev.ilkin@gmail.com>
2023-03-09 16:39:33 +0400

    New project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    plugins: [],
+
+    container: {
+      center: true,
+      
+    },
+    extend: {
+      fontFamily:{
+        "Roboto" : "'Roboto', sans-serif;"
+      }
+    },
+  },
+  
+}
